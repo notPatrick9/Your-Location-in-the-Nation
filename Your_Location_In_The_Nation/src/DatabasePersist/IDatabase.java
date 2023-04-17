3826dc588bdf5dbb675f595ed4c7b83d14ee92a8
Ryan-C208 <124527240+Ryan-C208@users.noreply.github.com>
2023-04-17 13:50:31 -0400

    Started to add datapase persistance layer

---
+++ Your_Location_In_The_Nation/src/DatabasePersist/IDatabase.java
@@ -0,0 +1,12 @@
+package DatabasePersist;
+
+import java.util.List;
+
+import edu.ycp.cs320.booksdb.model.Author;
+import edu.ycp.cs320.booksdb.model.Book;
+import edu.ycp.cs320.booksdb.model.Pair;
+
+public interface IDatabase {
+	
+	
+}
