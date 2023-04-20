9888af06cd4af0cd943eb018561abcf7a66765b9
Andy C <89643807+achinchilla1@users.noreply.github.com>
2023-04-20 11:26:20 -0400

    made servlet but there is one issue on line 61. also finished the questions JSP.

--- Your_Location_In_The_Nation/src/System/QuestionsServlet.java
+++ Your_Location_In_The_Nation/src/System/QuestionsServlet.java
@@ -1,7 +1,4 @@
-
-
 package System;
-
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -12,96 +9,82 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.derby.database.Database;
-
 import FakeDatabase.FakeData;
 import FindLocation.GetLocation;
 import LocationModel.Location;
 
+public class QuestionsServlet extends HttpServlet {
 
-public class QuestionsServlet extends HttpServlet{
-	
-	
-	FakeData Database = new FakeData();
-	List<Location> LocationList = new ArrayList<Location>();
-	
-	LocationList = Database.getLocationList();
-	 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
+    
+    private FakeData database;
+    private List<Location> LocationList;
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        database = new FakeData();
+        LocationList = database.getLocationList();
+    }
 
-		System.out.println("Questions Servlet: doGet");	
-		
-		// call JSP to generate empty form
-		req.getRequestDispatcher("/view/questions.jsp").forward(req, resp);
-	}
-	
-	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp)
-			throws ServletException, IOException {
-		
-		System.out.println("Question Servlet: doPost");
-		
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+
+        System.out.println("Questions Servlet: doGet");
+
    
-		// holds the error message text, if there is any
-		String errorMessage = null;
+        req.getRequestDispatcher("/view/questions.jsp").forward(req, resp);
+    }
 
-		// result of calculation goes here
-		Double result = null;
-		
-		// decode POSTed form parameters and dispatch to controller
-		try {
-		int CrimeRateFactor = Integer.parseInt(req.getParameter("CrimeRateFactor"));
-		int AveragesalaryFactor = Integer.parseInt(req.getParameter("AveragesalaryFactor"));
-		int CostOfLivingFactor = Integer.parseInt(req.getParameter("CostOfLivingFactor"));
-		
-			// check for errors in the form data before using is in a calculation
-		while (CrimeRateFactor + AveragesalaryFactor + CostOfLivingFactor != 10) {
-				errorMessage = "Please answer all the questions and make them equal to 10.";
-				
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
 
-			}
-		GetLocation Locationgetter = new GetLocation(CrimeRateFactor, AveragesalaryFactor, CostOfLivingFactor, LocationList);
-		//get the best Location based on user input
-		Location bestlocation = Locationgetter.FindBestLocation();   //copy 2 above
-			// otherwise, data is good, do the calculation
-			// must create the controller each time, since it doesn't persist between POSTs
-			// the view does not alter data, only controller methods should be used for that
-			// thus, always call a controller method to operate on the data
-			
-		} catch (NumberFormatException e) {
-			errorMessage = "Invalid int";
-		}
-		
+        System.out.println("Question Servlet: doPost");
+
+        // holds the error message text, if there is any
+        String errorMessage = null;
+
+        // result of calculation goes here
+        Location bestLocation = null;
+
+        // decode POSTed form parameters and dispatch to controller
+        try {
+            int crimeRateFactor = Integer.parseInt(req.getParameter("crimeRateFactor"));
+            int averageSalaryFactor = Integer.parseInt(req.getParameter("averageSalaryFactor"));
+            int costOfLivingFactor = Integer.parseInt(req.getParameter("costOfLivingFactor"));
+
+            if (crimeRateFactor + averageSalaryFactor + costOfLivingFactor != 10) {
+                errorMessage = "Please answer all the questions and make them equal to 10.";
+            } else {
+                GetLocation locationGetter = new GetLocation(crimeRateFactor, averageSalaryFactor, costOfLivingFactor, LocationList);
+             
+                bestLocation = locationGetter.FindBestLocation();
+            }
+        } catch (NumberFormatException e) {
+            errorMessage = "Invalid int";
+        }
+
 		// Add parameters as request attributes
 		// this creates attributes named "first" and "second for the response, and grabs the
 		// values that were originally assigned to the request attributes, also named "first" and "second"
 		// they don't have to be named the same, but in this case, since we are passing them back
 		// and forth, it's a good idea
-		req.setAttribute("CrimeRateFactor", req.getParameter("CrimeRateFactor"));
-		req.setAttribute("AveragesalaryFactor", req.getParameter("AveragesalaryFactor"));
-		req.setAttribute("CostOfLivingFactor", req.getParameter("CostOfLivingFactor"));
-		
-		
-		// add result objects as attributes
-		// this adds the errorMessage text and the result to the response
-		req.setAttribute("errorMessage", errorMessage);
-		req.setAttribute("result", result);
-		
-		// Forward to view to render the result HTML document
-		req.getRequestDispatcher("/view/questions.jsp").forward(req, resp);
-	}
+   
+        req.setAttribute("crimeRateFactor", req.getParameter("crimeRateFactor"));
+        req.setAttribute("averageSalaryFactor", req.getParameter("averageSalaryFactor"));
+        req.setAttribute("costOfLivingFactor", req.getParameter("costOfLivingFactor"));
+       
+     // this adds the errorMessage text and the result to the response
+        req.setAttribute("bestLocation", bestLocation);
+        req.setAttribute("errorMessage", errorMessage);
 
-	// gets double from the request with attribute named s
-	private Double getDoubleFromParameter(String s) {
-		if (s == null || s.equals("")) {
-			return null;
-		} else {
-			return Double.parseDouble(s);
-		}
-	}
+
+		// Forward to view to render the result HTML document
+        req.getRequestDispatcher("/view/questions.jsp").forward(req, resp);
+    }
+
 }
+
 
